c47937fa50ec37103f1cefacb1efb437cec2ca1d
oummadi <n.oummadi.e@gmail.com>
2024-11-09 13:32:06 +0100

    EVO WIP

--- app/src/main/java/com/plcoding/koinguide/MainActivity.kt
+++ app/src/main/java/com/plcoding/koinguide/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : ComponentActivity(), AndroidScopeComponent {
             KoinGuideTheme {
                 val viewModel = getViewModel<MainViewModel>()
                 viewModel.doNetworkCall()
-                Text("Koin Guide")
+                Text(hello)
             }
         }
     }
